ba057fac9242a9b88b28043e66a68c803b26551d
John Matzen <john@ayxia.com>
2015-11-04 23:17:20 -0600

    .

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
+echo ----- %1 
 rmdir /s/q build
 mkdir build
 cd build
 cmake -G"Visual Studio 14 2015 Win64" ..
-msbuild cryptopp.sln /t:cryptlib /p:Configuration=Release;Platform=x64
-msbuild cryptopp.sln /t:cryptlib /p:Configuration=Debug;Platform=x64
+rem msbuild cryptopp.sln /t:cryptlib /p:Configuration=%1;Platform=x64
+
--- mkpkg.py
+++ mkpkg.py
@@ -2,12 +2,25 @@ import json
 import sys
 import glob
 import os
+import shutil
+import zipfile
 
 
 spec= json.loads(open('package.json').read())
 
+if (len(sys.argv) < 2 or not sys.argv[1]):
+	exit(-1)
+
+zf = zipfile.ZipFile(sys.argv[1] + ".zip", "w")
+
 for pat in spec[sys.argv[1]]:
 	for file in glob.glob(pat[0]):
 		dst = pat[1] + '/' + os.path.basename(file)
-		print os.path.basename(file) + " -> " + dst
+		print file + " -> " + dst
+		# dstdir = os.path.dirname(dst)
+		# if (not os.path.exists(dstdir)):
+		# 	os.makedirs(dstdir)
+		# shutil.copy(file,dst)
+		zf.write(file,dst)
+
 
