dec679f3d253eb8b4f37abff8cfe73d2c089b52e
John Matzen <john@ayxia.com>
2015-11-04 23:32:37 -0600

    .

--- build.bat
+++ build.bat
@@ -3,5 +3,7 @@ rmdir /s/q build
 mkdir build
 cd build
 cmake -G"Visual Studio 14 2015 Win64" ..
-rem msbuild cryptopp.sln /t:cryptlib /p:Configuration=%1;Platform=x64
+msbuild cryptopp.sln /t:cryptlib /p:Configuration=%1;Platform=x64
+python mkpkg.py win32-x64-vc140-%1
+
 
